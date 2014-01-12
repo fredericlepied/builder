db4ed5d9ff159f763f107d63179cc60e14267146
Frederic Lepied <frederic.lepied@enovance.com>
2014-01-12 13:26:21 +0100

    first version

---
+++ builder.py
@@ -0,0 +1,155 @@
+#
+# Copyright (C) 2014 eNovance SAS <licensing@enovance.com>
+#
+# Author: Frederic Lepied <frederic.lepied@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+'''Module to build a dependency graph of shell commands like the make
+tool using the power of the Python language to be able to build more
+sophisticated rules.
+'''
+
+import commands
+import inspect
+import os.path
+import sys
+
+
+_RULES = {}
+_FIRST = None
+
+
+def mtime(path):
+    '''Return the modification time of the file or -1 if the file
+doesn't exist.
+'''
+    try:
+        return os.path.getmtime(path)
+    except os.error:
+        return -1
+
+
+def rule(target, build, *reqs):
+    '''Define a new rule in the ruleset. The rule defines how to build
+``target`` using the ``build`` function. ``reqs`` are the list of targets
+to satisfy before trying to build ``target``.
+
+These calls must be done between calls to ``init_vars`` and
+``process_targets``.
+'''
+    global _FIRST
+
+    if not _FIRST:
+        _FIRST = target
+
+    _RULES[target] = (reqs, build)
+
+
+def build_target(target):
+    '''Build a target by comparing modification times between the dependencies
+and the target recursively.
+'''
+    try:
+        rul = _RULES[target]
+        target_mtime = mtime(target)
+        rebuild = (target_mtime == -1)
+        for req in rul[0]:
+            req_mtime = mtime(req)
+            if req_mtime == -1:
+                if not build_target(req):
+                    return False
+                else:
+                    req_mtime = mtime(req)
+            if req_mtime > target_mtime:
+                rebuild = True
+        if rebuild:
+            if rul[1]:
+                return rul[1](target, rul[0])
+            else:
+                return True
+        else:
+            return True
+    except KeyError:
+        sys.stderr.write('*** UNKNOWN TARGET %s.\n' % target)
+        return False
+
+
+def cmd(*args):
+    '''Build the command from ``args``. Then display it before displaying its
+output. Return ``True`` if the command exists with 0 or ``False`` for other
+values.
+'''
+    cmdline = ' '.join(args)
+    sys.stderr.write('+ %s\n' % cmdline)
+    status, output = commands.getstatusoutput(cmdline)
+    print(output)
+    if status != 0:
+        sys.stderr.write('*** Error %d\n' % status)
+        return False
+    return True
+
+
+def run(*args):
+    '''Return a function that will launch a command when called. The arguments
+are split according to space to allow to pass a list or a string with spaces.
+'''
+    largs = sum([x.split() for x in args], [])
+
+    def do_run(_target, _reqs):
+        '''Generated function to run the command captured by the closure.'''
+        return cmd(*largs)
+    return do_run
+
+
+def process_targets():
+    '''Satisfy the targets passed on the command line or start with the first
+target defined in the rules file.
+
+This call must be placed at the end of the rules file.
+'''
+    if len(sys.argv[1:]) == 0:
+        if not _FIRST:
+            sys.stderr.write("*** NO TARGET.\n")
+            return 1
+        if not build_target(_FIRST):
+            return 1
+    else:
+        for target in sys.argv[1:]:
+            if not build_target(target):
+                return 1
+    return 0
+
+
+def process_vars(args):
+    '''Inject variable in the globals of the toplevel frame (in the form
+VAR=VAL) and return the list without these entries.
+'''
+    glob = inspect.stack()[-1][0].f_globals
+    for var in [x for x in args if '=' in x]:
+        key, val = var.split('=')
+        glob[key] = val
+    return [x for x in args if '=' not in x]
+
+
+def init_vars():
+    '''Process variable on the command line (in the form VAR=VAL) and inject
+them in the  globals of the top level frame. The sys.argv is changed by
+removing the variable arguments.
+
+This call must be placed after the declaration of variables in the rules file.
+'''
+    args = process_vars(sys.argv[1:])
+    sys.argv = [sys.argv[0], ] + args
+
+# builder.py ends here
---
+++ test_builder.py
@@ -0,0 +1,122 @@
+#
+# Copyright (C) 2014 eNovance SAS <licensing@enovance.com>
+#
+# Author: Frederic Lepied <frederic.lepied@enovance.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import os.path
+import tempfile
+import unittest
+
+import builder
+
+
+def touch(target, reqs):
+    return builder.cmd('touch', target)
+
+
+class TestBuilderTest(unittest.TestCase):
+
+    def setUp(self):
+        def cmd(*args):
+            self.run_cmd = ' '.join(args)
+            print(self.run_cmd)
+            return True
+
+        self.run_cmd = None
+        self.cmd = builder.cmd
+        builder.cmd = cmd
+
+        def mtime(path):
+            try:
+                t = self.time[path]
+            except KeyError:
+                t = -1
+            if isinstance(t, int):
+                return t
+            else:
+                return t.pop(-1)
+
+        self.time = {}
+        self.mtime = builder.mtime
+        builder.mtime = mtime
+
+    def tearDown(self):
+        builder._RULES = {}
+        builder._FIRST = None
+        builder.cmd = self.cmd
+        builder.mtime = self.mtime
+
+    def test_rule(self):
+        self.time = {'target': -1, 'req': 1}
+        builder.rule('target', touch, 'req')
+        self.assertTrue(builder.build_target('target'))
+        self.assertEqual(self.run_cmd, 'touch target')
+
+    def test_empty(self):
+        self.time = {'target': -1}
+        builder.rule('target', touch)
+        self.assertTrue(builder.build_target('target'))
+        self.assertEqual(self.run_cmd, 'touch target')
+
+    def test_first(self):
+        builder.rule('target', ('req',), touch)
+        self.assertEqual(builder._FIRST, 'target')
+
+    def test_complex(self):
+        self.time = {'target': -1, 'req': [-1, 1]}
+        builder.rule('target', touch, 'req')
+        builder.rule('req', touch)
+        self.assertTrue(builder.build_target('target'))
+        self.assertEqual(self.run_cmd, 'touch target')
+
+    def test_unknown_target(self):
+        self.assertFalse(builder.build_target('target'))
+
+    def test_unknown_dep(self):
+        builder.rule('target', touch, 'req')
+        self.assertFalse(builder.build_target('target'))
+
+    def test_already_built(self):
+        self.time = {'target': 1}
+        builder.rule('target', touch)
+        self.assertTrue(builder.build_target('target'))
+
+    def test_mtime(self):
+        temp = tempfile.NamedTemporaryFile()
+        self.assertTrue(self.mtime(temp.name) != -1)
+        temp.close()
+
+    def test_mtime_does_not_exist(self):
+        path = '/'
+        while os.path.exists(path):
+            path = path + '0'
+        self.assertTrue(self.mtime(path) == -1)
+
+    def test_cmd(self):
+        self.assertTrue(self.cmd('true'))
+
+    def test_failing_cmd(self):
+        self.assertFalse(self.cmd('false'))
+
+    def test_run(self):
+        self.time = {'target': -1}
+        builder.rule('target', builder.run('touch', 'target'))
+        self.assertTrue(builder.build_target('target'))
+        self.assertEqual(self.run_cmd, 'touch target')
+
+if __name__ == "__main__":
+    unittest.main()
+
+# test_builder.py ends here
